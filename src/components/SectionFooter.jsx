7d641136b74599033a5272448ff35e1de66358ed
snakepatch53 <snakepatch53@gmail.com>
2024-02-01 02:00:14 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	renamed:    dist/assets/index-73074491.js -> dist/assets/index-8e60b273.js
    	modified:   dist/index.html
    	modified:   src/components/SectionFooter.jsx
    
    Changes not staged for commit:
    	modified:   src/components/SectionFooter.jsx
    	modified:   src/components/SectionHero.jsx
    	modified:   src/components/SectionNavBar.jsx
    	modified:   src/components/SectionResume.jsx
    	modified:   src/components/SectionSkills.jsx
    	modified:   src/components/SectionWorks.jsx

--- src/components/SectionFooter.jsx
+++ src/components/SectionFooter.jsx
@@ -9,7 +9,7 @@ export default function SectionFooter({ info }) {
         <div id="works" className=" mx-auto m-auto h-[300px]  mt-16 sm:h-[250px]">
             <div className=" bg-[var(--c2-bg)] h-full flex flex-col gap-8 items-center justify-between p-10 sm:p-7">
                 <h2 data-aos="zoom-out" className=" font-bold text-5xl sm:text-3xl">
-                    Hableame
+                    Háblame
                 </h2>
                 <div className=" flex items-center justify-center gap-8 sm:gap-5">
                     {social.map((item) => (
