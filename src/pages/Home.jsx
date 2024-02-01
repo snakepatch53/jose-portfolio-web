c7ca537b21417f5e771c34017b6236829952f188
snakepatch53 <snakepatch53@gmail.com>
2024-01-31 19:34:22 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   data.json
    	new file:   public/portfolio/ists.png
    	modified:   src/App.jsx
    	new file:   src/components/Contact.jsx
    	new file:   src/components/Footer.jsx
    	new file:   src/components/Hero.jsx
    	new file:   src/components/NavBar.jsx
    	new file:   src/components/Resume.jsx
    	new file:   src/components/Skills.jsx
    	new file:   src/components/Work.jsx
    	deleted:    src/components/contact/Contact.jsx
    	deleted:    src/components/footer/Footer.jsx
    	deleted:    src/components/hero/Hero.jsx
    	deleted:    src/components/navbar/NavBar.jsx
    	deleted:    src/components/resume/Resume.jsx
    	deleted:    src/components/skills/Skills.jsx
    	deleted:    src/components/work/Work.jsx
    	modified:   src/data/data.js
    	modified:   src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,29 @@
 import React, { useState } from "react";
-import Contact from "../components/contact/Contact";
-import Footer from "../components/footer/Footer";
-import Hero from "../components/hero/Hero";
-import NavBar from "../components/navbar/NavBar";
-import Resume from "../components/resume/Resume";
-import Skills from "../components/skills/Skills";
-import Work from "../components/work/Work";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import NavBar from "../components/NavBar";
+import Resume from "../components/Resume";
+import Skills from "../components/Skills";
+import Work from "../components/Work";
+import info from "../../data.json";
 
 const Home = () => {
-  const [darkMode, setDarkMode] = useState(false);
-  function toggleDarkMode() {
-    setDarkMode((prevDarkMode) => !prevDarkMode);
-  }
-  return (
-    <>
-      <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <Hero darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <Skills darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <Work darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <Resume darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <Contact darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <Footer darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-    </>
-  );
+    const [darkMode, setDarkMode] = useState(false);
+    function toggleDarkMode() {
+        setDarkMode((prevDarkMode) => !prevDarkMode);
+    }
+    return (
+        <>
+            <NavBar info={info} darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            <Hero info={info} darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            <Skills info={info} darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            <Work info={info} darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            <Resume info={info} darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            <Contact info={info} darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            <Footer info={info} darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+        </>
+    );
 };
 
 export default Home;
